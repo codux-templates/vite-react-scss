2ea6107f2f80a96cfd0d13c08e680cd8df5da976
Shaked Erinberg <shakede@wix.com>
2023-02-09 12:17:55 +0200

    upgrade dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
     return (
         <div className={styles.App}>
             <div>
+               <div></div>
                 <a href="https://vitejs.dev" target="_blank">
                     <img src={viteLogo} className={styles.logo} alt="Vite logo" />
                 </a>
