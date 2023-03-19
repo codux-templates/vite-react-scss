07745e06b91d2329859e18b5fdb77035554564f6
cario <cario@wix.com>
2023-03-19 18:18:55 +0200

    chore: fix app appearance; css, favicon

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
                 </a>
             </div>
             <h1>Vite + React</h1>
-            <div className={styles.react}>
+            <div className={styles.card}>
                 <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
