05491fdb2368dc8bef86a50bdb0acd75646d0e39
Alexey Lebedev <alisey@users.noreply.github.com>
2023-05-18 16:23:05 +0300

    fix: KB article about new component template configuration (#11)

--- src/_codux/component-templates/default/new-component.tsx
+++ src/_codux/component-templates/default/new-component.tsx
@@ -7,7 +7,7 @@ export interface NewComponentProps {
 
 /**
  * This component was created using Codux's Default new component template.
- * To create custom component templates, see https://help.codux.com/kb/en/article/configuration-for-new-components-and-templates
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const NewComponent = ({ className }: NewComponentProps) => {
     return <div className={classNames(styles.root, className)}>NewComponent</div>;
