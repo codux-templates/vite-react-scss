b2eef71004fd6729dc2ed5a09837e187f73ed445
Sagiv Dayan <sagiv.keeper@gmail.com>
2024-03-12 12:37:15 +0200

    chore: change main board; add grid and stack component templates

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -4,4 +4,8 @@ import App from '../../../App';
 export default createBoard({
     name: 'App',
     Board: () => <App />,
+    environmentProps: {
+        windowWidth: 1024,
+        windowHeight: 768,
+    },
 });
